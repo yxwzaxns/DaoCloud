91e9b944b4e3ed224e1811abc6fd84c5b1e60bf4
yxwzaxns <yxwzaxns@gmail.com>
2015-12-02 12:30:02 +0800

    first

--- doc/influxdb/code/myapp.rb
+++ doc/influxdb/code/myapp.rb
@@ -5,37 +5,36 @@ require 'json'
 
 module Sinatra
   class Base
-    set :server, %w[thin mongrel webrick]
+    set :server, %w(thin mongrel webrick)
     set :bind, '0.0.0.0'
     set :port, 8080
     set :views, File.dirname('.') + '/views'
   end
 end
 
+host=ENV['INFLUXDB_PORT_8086_TCP_ADDR']
+username=ENV['INFLUXDB_USERNAME']
+password=ENV['INFLUXDB_PASSWORD']
+port=ENV['INFLUXDB_PORT_8086_TCP_PORT']
+database="db"
 
-host=ENV['MYSQL_PORT_3306_TCP_ADDR']
-username=ENV['MYSQL_USERNAME']
-password=ENV['MYSQL_PASSWORD']
-port=ENV['MYSQL_PORT_3306_TCP_PORT']
-database=ENV['MYSQL_INSTANCE_NAME']
-
-$create_database_uri="http://#{host}=port}/query?u=#{username}&p=#{password}&q=create database #{database}"
-$query_uri="http://#{host}=port}/query?u=#{username}&p=#{password}&db=#{database}&q=select * from sin"
-$write_uri="http://#{host}=port}/write?u=#{username}&p=#{password}&db=#{database}"
+$create_database_uri = "http://#{host}:#{port}/query?u=#{username}&p=#{password}&q=create database #{database}"
+$query_uri = "http://#{host}:#{port}/query?u=#{username}&p=#{password}&db=#{database}&q=select * from sin"
+$write_uri = "http://#{host}:#{port}/write?u=#{username}&p=#{password}&db=#{database}"
 
 get '/' do
-  n=0
+  n = 0
   Net::HTTP.get(URI($create_database_uri))
   Array(1..90).each do |e|
-      Net::HTTP.start(URI($write_uri).host,URI($write_uri).port) do |http|
-        http.request_post($write_uri,"sin,val=#{e} value=#{Math.sin(Math::PI/180*e)}")
-        n+=1
-      end
+    Net::HTTP.start(URI($write_uri).host, URI($write_uri).port) do |http|
+      http.request_post($write_uri, "sin,val=#{e} value=#{Math.sin(Math::PI / 180 * e)}")
+      n += 1
+    end
   end
-  erb :index,:locals => {:n => n}
+  erb :index, locals: { n: n }
 end
 
 get '/info' do
-  res=JSON.parse(Net::HTTP.get(URI($query_uri)))
-  erb :info,:locals => {:info => res}
+  res = JSON.parse(Net::HTTP.get(URI($query_uri)))
+  erb :info, locals: { info: res }
 end
